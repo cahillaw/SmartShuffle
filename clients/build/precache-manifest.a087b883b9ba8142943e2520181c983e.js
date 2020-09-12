045f68e32cc9c9e1c60b4d96b91b12d039cadbab
Andy Cahill <Cahillawx@gmail.com>
2020-09-11 17:58:54 -0700

    added build folder

---
+++ clients/build/precache-manifest.a087b883b9ba8142943e2520181c983e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2afc128cfcd08a0008ced90b33cb15c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d34a812f8b49a1a3ebb",
+    "url": "/static/css/main.e41f92ec.chunk.css"
+  },
+  {
+    "revision": "d7a1f19400f17120fdb3",
+    "url": "/static/js/2.3fe6ff1a.chunk.js"
+  },
+  {
+    "revision": "88cbcb55f867c7e879dbc5e47becbf3b",
+    "url": "/static/js/2.3fe6ff1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d34a812f8b49a1a3ebb",
+    "url": "/static/js/main.3ae7c1b8.chunk.js"
+  },
+  {
+    "revision": "3bf5c542dd7c1c0410c2",
+    "url": "/static/js/runtime-main.af21c46c.js"
+  },
+  {
+    "revision": "9b4ac1e86e870d30815cbe485be97daa",
+    "url": "/static/media/playbig.9b4ac1e8.png"
+  }
+]);
